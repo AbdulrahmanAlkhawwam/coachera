66d6ac5a7cc77b9d22a1b4ef87d04b29d0570afe
Abdulrahman Alkhawwam <abd.kwm@gmail.com>
2025-06-29 13:29:41 +0300

    add search component

---
+++ lib/features/search/presentation/widgets/search_card.dart
@@ -0,0 +1,20 @@
+import 'package:coachera/core/components/custom_input.dart';
+import 'package:coachera/core/constants/routes.dart';
+import 'package:coachera/core/utils/app_context.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+
+class SearchCard extends StatelessWidget {
+  const SearchCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomInput(
+      onTap: () => context.push(Routes.search),
+      isEnabled: false,
+      prefixIcon: TablerIcons.search,
+      suffixIcon: TablerIcons.adjustments_horizontal,
+      hint: "Search your course ...",
+    );
+  }
+}
