b9c47288f21a1f2c5217c49b3dbb30d9737e5a1b
Abdulrahman Alkhawwam <abd.kwm@gmail.com>
2025-08-19 14:31:08 +0300

    Refactor: Update FAQ screen to use localization keys directly
    - Modified `faq_screen.dart` to use localization keys for questions and answers instead of pre-translated strings.
    - Removed an unnecessary level of nesting for FAQ localization keys in `en.json`.

--- lib/features/home/presentation/pages/faq_screen.dart
+++ lib/features/home/presentation/pages/faq_screen.dart
@@ -1,116 +1,116 @@
-import 'package:coachera/core/localization/keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 
+import '../../../../core/localization/keys.g.dart';
 import '../../../../core/utils/app_context.dart';
 
-final List<Map<String, dynamic>> faq = [
+final List<Map<String, dynamic>> faqKeys = [
   {
-    "Question": LocaleKeys.screens_FAQ_q1_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q1_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q1_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q1_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q1_Answer_a1,
+      LocaleKeys.screens_FAQ_q1_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q2_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q2_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q2_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q2_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q2_Answer_a1,
+      LocaleKeys.screens_FAQ_q2_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q3_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q3_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q3_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q3_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q3_Answer_a1,
+      LocaleKeys.screens_FAQ_q3_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q4_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q4_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q4_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q4_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q4_Answer_a1,
+      LocaleKeys.screens_FAQ_q4_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q5_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q5_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q5_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q5_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q5_Answer_a1,
+      LocaleKeys.screens_FAQ_q5_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q6_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q6_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q6_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q6_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q6_Answer_a1,
+      LocaleKeys.screens_FAQ_q6_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q7_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q7_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q7_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q7_Answer_a2.tr(),
-      LocaleKeys.screens_FAQ_q7_Answer_a3.tr(),
+      LocaleKeys.screens_FAQ_q7_Answer_a1,
+      LocaleKeys.screens_FAQ_q7_Answer_a2,
+      LocaleKeys.screens_FAQ_q7_Answer_a3,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q8_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q8_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q8_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q8_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q8_Answer_a1,
+      LocaleKeys.screens_FAQ_q8_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q9_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q9_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q9_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q9_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q9_Answer_a1,
+      LocaleKeys.screens_FAQ_q9_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q10_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q10_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q10_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q10_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q10_Answer_a1,
+      LocaleKeys.screens_FAQ_q10_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q11_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q11_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q11_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q11_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q11_Answer_a1,
+      LocaleKeys.screens_FAQ_q11_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q12_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q12_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q12_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q12_Answer_a2.tr(),
-      LocaleKeys.screens_FAQ_q12_Answer_a3.tr(),
+      LocaleKeys.screens_FAQ_q12_Answer_a1,
+      LocaleKeys.screens_FAQ_q12_Answer_a2,
+      LocaleKeys.screens_FAQ_q12_Answer_a3,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q13_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q13_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q13_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q13_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q13_Answer_a1,
+      LocaleKeys.screens_FAQ_q13_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q14_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q14_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q14_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q14_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q14_Answer_a1,
+      LocaleKeys.screens_FAQ_q14_Answer_a2,
     ]
   },
   {
-    "Question": LocaleKeys.screens_FAQ_q15_Question.tr(),
+    "Question": LocaleKeys.screens_FAQ_q15_Question,
     "Answer": [
-      LocaleKeys.screens_FAQ_q15_Answer_a1.tr(),
-      LocaleKeys.screens_FAQ_q15_Answer_a2.tr(),
+      LocaleKeys.screens_FAQ_q15_Answer_a1,
+      LocaleKeys.screens_FAQ_q15_Answer_a2,
     ]
   },
 ];
@@ -132,11 +132,11 @@ class _FAQScreenState extends State<FAQScreen>
       appBar: AppBar(
         leading: IconButton(
           onPressed: () => Navigator.canPop(context) ? context.pop() : null,
-          icon: Icon(context.isLTR
-              ? TablerIcons.chevron_left
-              : TablerIcons.chevron_right),
+          icon: Icon(
+            context.isLTR ? TablerIcons.chevron_left : TablerIcons.chevron_right,
+          ),
         ),
-        title: Text("FAQ"),
+        title: Text("FAQ"), // <-- you can also localize this if needed
       ),
       body: ListView.separated(
         separatorBuilder: (context, index) => const SizedBox(height: 16),
@@ -146,7 +146,7 @@ class _FAQScreenState extends State<FAQScreen>
           right: 16.0,
           bottom: 16 + context.bottomPadding,
         ),
-        itemCount: faq.length,
+        itemCount: faqKeys.length,
         itemBuilder: (context, index) {
           final isExpanded = _expandedIndex == index;
 
@@ -155,63 +155,40 @@ class _FAQScreenState extends State<FAQScreen>
               borderRadius: BorderRadius.circular(8),
             ),
             child: ExpansionTile(
-                onExpansionChanged: (expanded) =>
-                    setState(() => _expandedIndex = expanded ? index : null),
-                initiallyExpanded: isExpanded,
-                trailing:
-                    Icon(isExpanded ? TablerIcons.plus : TablerIcons.minus),
-                title: Text(faq[index]['Question']!),
-                children: List.generate(
-                  faq[index]['Answer'].length,
-                  (answerIndex) => Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: CircleAvatar(
-                            radius: 3,
-                            backgroundColor: context.colors.onPrimaryContainer
+              onExpansionChanged: (expanded) =>
+                  setState(() => _expandedIndex = expanded ? index : null),
+              initiallyExpanded: isExpanded,
+              trailing: Icon(isExpanded ? TablerIcons.plus : TablerIcons.minus),
+              title: Text(faqKeys[index]['Question'].toString().tr()),
+              children: List.generate(
+                (faqKeys[index]['Answer'] as List).length,
+                    (answerIndex) => Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          radius: 3,
+                          backgroundColor:
+                          context.colors.onPrimaryContainer.withAlpha(160),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          (faqKeys[index]['Answer'][answerIndex] as String).tr(),
+                          style: context.textTheme.bodyMedium?.copyWith(
+                            color: context.colors.onPrimaryContainer
                                 .withAlpha(160),
                           ),
                         ),
-                        Expanded(
-                          child: Text(
-                            faq[index]['Answer'][answerIndex],
-                            style: context.textTheme.bodyMedium?.copyWith(
-                                color: context.colors.onPrimaryContainer
-                                    .withAlpha(160)),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                )
-                // List.generate(_['lessons'].length, (lessonIndex) {
-                // final lesson = section['lessons'][lessonIndex];
-                // return ListTile(
-                //   contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-                //   title: Text(
-                //     lesson['title'],
-                //     style: const TextStyle(color: Colors.white),
-                //   ),
-                //   subtitle: Text(
-                //     lesson['duration'],
-                //     style: const TextStyle(color: Color(0xFFA7A7A7)),
-                //   ),
-                //   trailing: const Icon(
-                //     Icons.play_circle_fill,
-                //     color: Color(0xFFFFBD12),
-                //   ),
-                //   onTap: () {
-                //     Handle lesson tap
-                // },
-                // );
-                // }),
                 ),
-            /*   ),
-         */
+              ),
+            ),
           );
         },
       ),
