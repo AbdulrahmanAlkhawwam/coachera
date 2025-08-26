46a8d0aeb0e20b767dea8fce8af6a03f8a31dfd9
Abdulrahman Alkhawwam <abd.kwm@gmail.com>
2025-08-26 15:00:47 +0300

    Refactor: Remove unused instructor and category features
    This commit removes the following unused features and their associated files:
    - Instructor details screen (`instructor_details_screen.dart`)
    - Instructor course card, description, and list widgets
    - Recommended courses screen and parameters (`recommended_courses_screen.dart`, `recommended_courses_param.dart`)
    - Category-related files in the `lesson` feature (entities, models, blocs, repositories, use cases)
    - Search screen (`search_screen.dart`)
    - `categories_list.dart` widget from the `instructor` feature

--- lib/features/learningPath/domain/params/recommended_courses_param.dart
+++
@@ -1,9 +0,0 @@
-import '../../../home/presentation/widgets/filter_sheet.dart';
-
-class RecommendedCoursesParam {
-  int page;
-
-  FilterData filter;
-
-  RecommendedCoursesParam({required this.page, required this.filter});
-}
