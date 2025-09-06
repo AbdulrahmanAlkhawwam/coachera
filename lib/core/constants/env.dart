4886648f5c60d4eb6972aba0514aa73d2ee0123e
Abdulrahman Alkhawwam <abd.kwm@gmail.com>
2025-09-06 03:54:47 +0300

    Refactor: Comment out unused Firebase configuration variables in env.dart

--- lib/core/constants/env.dart
+++ lib/core/constants/env.dart
@@ -2,4 +2,14 @@ class Env {
   static const host = String.fromEnvironment("HOST");
   static const baseUrl = String.fromEnvironment("BASEURL");
   static const stripeKey = String.fromEnvironment("STRIPE_KEY");
+
+  /// FireBase
+  // static const apiKey = String.fromEnvironment("API_KEY");
+  // static const authDomain = String.fromEnvironment("AUTH_DOMAIN");
+  // static const projectId = String.fromEnvironment("PROJECT_ID");
+  // static const storageBucket = String.fromEnvironment("STORAGE_BUCKET");
+  // static const messagingSenderId = String.fromEnvironment("MESSAGING_SENDER_ID");
+  // static const appId = String.fromEnvironment("APP_ID");
+  // static const measurementId = String.fromEnvironment("MEASUREMENT_ID");
+
 }
